136106548353567393a768be29cf8c26d3f056c5
Shane Cory Selig <shanecs@me.com>
2018-05-18 20:54:26 -0500

    Installed Template

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,12 @@
+// Stylesheets
+import 'bootstrap';
+import '../styles/bootstrap-theme.scss';
+import 'font-awesome-sass-loader';
 import '../styles/app.scss';
+
+// Javascript
+// import $ from 'jquery';
+// window.jQuery = $;
+// window.$ = $;
+import './plugins.js';
+import './main.js';
