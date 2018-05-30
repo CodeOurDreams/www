428d3f91ebc02dd1e8a9ef37d203dd0c513fd66d
Shane Cory Selig <shanecs@me.com>
2018-05-29 19:40:20 -0500

    Added Template and Supporting Files

---
+++ src/javascript/app.js
@@ -0,0 +1,8 @@
+// Stylesheets
+// import 'font-awesome/scss/font-awesome.scss';
+// @import url('font-awesome.min.css');
+import '../styles/app.scss';
+
+// // Javascript
+// import './plugins.js';
+// import './main.js';
---
+++ src/languages/english.js
@@ -0,0 +1,6 @@
+module.exports = {
+    orgName: 'Code Our Dreams',
+    email: 'info@codeourdreams.org',
+    phone: '(410) 790-5273‬',
+    address: '445 East Ohio Street, Apartment 2605, Chicago, IL 60611'
+}
