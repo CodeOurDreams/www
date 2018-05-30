c3332b155c8d51bcb43979b93eebb307b9f05be8
Shane Cory Selig <shanecs@me.com>
2018-05-30 15:45:20 -0500

    Updated Dependencies and Package.json Scripts

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,11 +1,7 @@
 // Stylesheets
 import 'font-awesome/scss/font-awesome.scss';
-import '../styles/app.scss';
+import '..npm-/styles/app.scss';
 
-// // Javascript
-import('./vendors/jquery.scrolly.js');
-import('./vendors/jquery.dropotron.js');
-import 'jquery.scrollex';
-import './plugins.js';
+// Javascript
 import './utils.js';
 import './main.js';
