bc1bc90ab355220f02dff5163eeecc9dc9ebff93
Shane Cory Selig <shanecs@me.com>
2018-06-05 23:04:25 -0500

    Improved Modal
    Ensured that the modal is the top element, and prevented scrolling when open.

--- src/javascript/custom.js
+++ src/javascript/custom.js
@@ -1,29 +1,43 @@
 /*
  * BIO MODAL
  */
-// Get the modal
-var modalBio = document.getElementById('modalBio');
-
 // Open Modal
 var modalBioOpen = document.getElementsByClassName('modalBioOpen');
 for (var i = 0; i < modalBioOpen.length; ++i) {
-    modalBioOpen[i].onclick = function(e) {
-        document.getElementById('modalBioImg').src = e.target.parentNode.childNodes[0].src;
-        document.getElementById('modalBioName').innerText = e.target.parentNode.childNodes[1].innerText;
-        document.getElementById('modalBioTitle').innerText = e.target.parentNode.childNodes[2].innerText;
-        document.getElementById('modalBioBio').innerText = e.target.parentNode.getAttribute('bio');
-        modalBio.style.display = 'block';
-    }
+    modalBioOpen[i].onclick = openModal;
 }
 
 // Close the Modal
-document.getElementById('modalBioClose').onclick = function() {
-    modalBio.style.display = 'none';
-}
+document.getElementById('modalBioClose').onclick = closeModal;
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modalBio) {
-        modalBio.style.display = 'none';
+        closeModal();
     }
+}
+
+function openModal(event) {
+    // Set Data
+    document.getElementById('modalBioImg').src = event.target.parentNode.childNodes[0].src;
+    document.getElementById('modalBioName').innerText = event.target.parentNode.childNodes[1].innerText;
+    document.getElementById('modalBioTitle').innerText = event.target.parentNode.childNodes[2].innerText;
+    document.getElementById('modalBioBio').innerText = event.target.parentNode.getAttribute('bio');
+
+    // Prevent Scroll
+    document.getElementsByTagName('body')[0].scroll = 'no';
+    document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+
+    // Show Modal
+    document.getElementById('modalBio').style.display = 'block';
+    
+}
+
+function closeModal() {
+    // Prevent Scroll
+    document.getElementsByTagName('body')[0].scroll = 'initial';
+    document.getElementsByTagName('body')[0].style.overflow = 'initial';
+    
+    // Show Modal
+    document.getElementById('modalBio').style.display = 'none';
 }
